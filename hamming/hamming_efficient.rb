dd0b3ee57ad9600454606ed7dad9a912d0c63d93
Dearg OBartuin <21969863+MrDearg@users.noreply.github.com>
2018-08-28 00:00:17 +0100

    fixed hamming indentation to spaces not tabs

--- hamming/hamming_efficient.rb
+++ hamming/hamming_efficient.rb
@@ -5,12 +5,12 @@
 ##
 	
 class Hamming
-	def self.compute(strand_1, strand_2)
-		raise ArgumentError if strand_1.length != strand_2.length
-		s1 = strand_1.chars
-		s2 = strand_2.chars
-		s1.zip(s2).count { |a, b| a != b }
-	end
+  def self.compute(strand_1, strand_2)
+    raise ArgumentError if strand_1.length != strand_2.length
+    s1 = strand_1.chars
+    s2 = strand_2.chars
+    s1.zip(s2).count { |a, b| a != b }
+  end
 end
 
 p Hamming.compute('ABCDEF', 'ABCDEF')
