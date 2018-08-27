95e06fc1ee6516c090fa49a2d5385b079193ab55
Dearg OBartuin <21969863+MrDearg@users.noreply.github.com>
2018-08-27 23:44:59 +0100

    updated hamming to remove hamming_count and stop using instanve variables

--- hamming/hamming_efficient.rb
+++ hamming/hamming_efficient.rb
@@ -1,17 +1,16 @@
-=begin
-This time without relying upon the index of two array comparisons and using the 
-Array#Zip method to compare strings
-=end
+##
+# This time without relying upon the index of two array comparisons and using the 
+# Array#Zip method to compare strings.
+# Also using Enumerable#count on Zip to replace @hamming_count variable
+##
 	
 class Hamming
 	def self.compute(strand_1, strand_2)
 		raise ArgumentError if strand_1.length != strand_2.length
-		@s1 = strand_1.chars
-		@s2 = strand_2.chars
-		@hamming_count = 0
-		@s1.zip(@s2).count { |a, b| @hamming_count =+ 1 if a == b }
-		@hamming_count
+		s1 = strand_1.chars
+		s2 = strand_2.chars
+		s1.zip(s2).count { |a, b| a != b }
 	end
 end
 
-p Hamming.compute('GATACAF', 'GATACAB')
+p Hamming.compute('ABCDEF', 'ABCDEF')
